58cc0ea424156ebfb3d5b49e96b3223a9d5e7525
quang nguyen minh <nguyenminhquangcn1@gmail.com>
2024-05-19 23:07:12 +0700

    Unify main.c

--- source/main.c
+++ source/main.c
@@ -16,10 +16,7 @@
 #define IS_TX							TRUE
 #define GREEN_LED						PB10
 #define USB_ENABLE_PIN					PB5
-
-#define STATUS_MAX_RT					0b10000
-#define STATUS_TX_DS					0b100000
-#define STATUS_RX_DR					0b1000000
+#define FIFO_STATUS_RX_FULL				0b10
 /***********************************************************************************************************************
  * Prototypes
  **********************************************************************************************************************/
@@ -31,9 +28,7 @@ volatile int irq = 0;
 uint32_t timer = 0;
 uint8_t Address[5];
 uint8_t RxData[100];
-uint8_t TxData[] = "Hello world";
-uint8_t check[100] = 0;
-uint8_t after[100] = 0;
+uint8_t TxData[] = "ABCDEFGHIKLMNOPQRSTVXYZ123456789";
 int i= 0;
 int test = 0;
 int dem = 0;
@@ -44,7 +39,7 @@ int rx_dr = 0;
 /***********************************************************************************************************************
  * Code
  **********************************************************************************************************************/
-void main()
+ void main()
 {
 	/* Clock initialization */
 	CLOCK_Init(8);
@@ -66,6 +61,7 @@ void main()
 	/* GPIO initialization */
 	GPIO_SetInPut(NRF24L01_IRQ_PIN, Input_doubleing);
 	GPIO_SetOutPut(GREEN_LED, General_Push_Pull);
+	GPIO_SetOutPut(PC13, General_Push_Pull);
 	GPIO_PinHigh(GREEN_LED);
 	GPIO_SetOutPut(USB_ENABLE_PIN, General_Push_Pull);
 	GPIO_PinLow(USB_ENABLE_PIN);
@@ -87,51 +83,37 @@ void main()
 #else
 	nRF24L01_RxMode(Address, 0);
 #endif
-	test = nRF24L01_Read1Byte(STATUS_REG);
 	while (1)
 	{
 		timer = micros();
 #if (IS_TX == TRUE)
-		//nRF24L01_Command(FLUSH_TX);
 		nRF24L01_Transmit(TxData);
-
 		GPIO_PinToggle(GREEN_LED);
-		dem++;
-
 #else
-		if (nRF24L01_DataAvailable(1) == 1)
+		test = nRF24L01_Read1Byte(STATUS_REG);
+		if ((test & 0b1110) != 0b1110)
 		{
+			nRF24L01_Disable();
+			for (i=0;i<=255;i++)
+				RxData[i]=0;
+			nRF24L01_Write1Byte(STATUS_REG, STATUS_RX_DR);
 			nRF24L01_ReceiveData(RxData);
-			USB_SendString(1, RxData, strlen((char *)RxData));
-			GPIO_PinToggle(GREEN_LED);
+
+			while ((test & FIFO_STATUS_RX_FULL)
+				   == FIFO_STATUS_RX_FULL)
+			{
+				test = nRF24L01_Read1Byte(FIFO_STATUS_REG);
+				nRF24L01_ReceiveData(&RxData[32]);
+			}
+			nRF24L01_Command(FLUSH_RX);
+			nRF24L01_RxMode(Address, 0);
+			success++;
+			if (RxData[1] == 'B')
+				success ++;
+			GPIO_PinToggle(PC13);
+			check = 1;
 		}
 #endif
-		if (GPIO_ReadPIN(NRF24L01_IRQ_PIN) == 0)
-		{
-			check[i] = nRF24L01_Read1Byte(STATUS_REG);
-			test = check [i];
-			if ((test & STATUS_MAX_RT) == STATUS_MAX_RT)
-			{
-				nRF24L01_Write1Byte(STATUS_REG, STATUS_MAX_RT);
-				after[i] = nRF24L01_Read1Byte(STATUS_REG);
-				nRF24L01_TxMode(Address, 0);
-				nRF24L01_Disable();
-				max ++;
-			}
-			if ((test & STATUS_TX_DS) == STATUS_TX_DS)
-			{
-				nRF24L01_Write1Byte(STATUS_REG, 0x10);
-				nRF24L01_Read1Byte(STATUS_REG);
-				nRF24L01_TxMode(Address, 0);
-				tx ++;
-				test = 0;
-			}
-			if ((test & STATUS_RX_DR) == STATUS_RX_DR)
-			{
-				rx_dr++;
-			}
-			i++;
-		}
 		while (micros() - timer < 1000000);
 	}
 }
@@ -141,15 +123,26 @@ void EXTI3_IRQHandler(void)
 {
 	EXTI->PR |= BIT_3;
 	irq = nRF24L01_Read1Byte(STATUS_REG);
+	nRF24L01_Disable();
+#if (IS_TX == TRUE)
 	if ((irq & STATUS_MAX_RT) == STATUS_MAX_RT)
 	{
 		nRF24L01_Write1Byte(STATUS_REG, STATUS_MAX_RT);
+		nRF24L01_Enable();
 	}
-	else
+	else if ((irq & STATUS_TX_DS) == STATUS_TX_DS)
 	{
 		nRF24L01_TxMode(Address, 0);
+		GPIO_PinToggle(PC13);
 		success++;
 	}
+#else
+	if ((irq & STATUS_RX_DR) == STATUS_RX_DR)
+	{
+		irq++;
+		check = 0;
+	}
+#endif
 }
 /***********************************************************************************************************************
  * EOF
